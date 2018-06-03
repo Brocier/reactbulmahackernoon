0887b6c933b756cd7e3dc009b2799964fd8257a4
Brocier <jschoe04@gmail.com>
2018-06-03 16:51:46 -0400

    finished day one in the hackernoon blog

---
+++ src/app/layout/Footer.js
@@ -0,0 +1,24 @@
+//===========// Footer.js // ===========//
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <div className="content has-text-centered">
+        <p>
+          Built on
+          <strong>React
+          </strong>
+          with
+          <strong>Bulma</strong>
+          by
+          <a href="https://github.com/brocier">Joshua Schoenfeld</a>
+          <br/>
+          Copyright 2018
+        </p>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
